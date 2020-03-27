bdc5ef6b07c6ba4ada74158f455423a2acb1c49e
Vinicius Mendite <vinicius.miff@gmail.com>
2020-03-27 16:46:11 -0300

    Funcionalidades avançadas como validação e testes TDD

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -8,13 +9,36 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(), // Validando que o nome é uma String e é obrigatória
+        email: Joi.string().required().email(), // Validand0 que o email é uma String, é obrigatório e é um email
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 routes.post('/sessions', SessionController.create);
 
